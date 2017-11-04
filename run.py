c0c4f30f2d694d4e36fa031851ebbbf7272eb7ff
ErikBoesen <me@erikboesen.com>
2017-11-03 22:42:53 -0400

    Arg handling, image processing improvements

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 from PIL import Image
 import numpy as np
+import argparse
 
-im = Image.open('orig.jpg')
+parser = argparse.ArgumentParser(description='Generate pixel portraits from an image.')
+parser.add_argument('file', type=str, help='Input file.')
+parser.add_argument('-out', type=str, default='', help='Output file.')
+args = parser.parse_args()
+
+im = Image.open(args.file)
 im = im.resize((64, 64))
 
-r, g, b = np.array(im).T
+_, g, _ = np.array(im).T
+g = map(round, g)
+g *= 64
 
-a = np.zeros_like(b)
-a[b < 200] = 255
 
-im = Image.fromarray(np.dstack([item.T for item in (r, g, b)]))  # , a)]))
-im.save('mod.png')
+im = Image.fromarray(np.dstack((g.T, g.T, g.T)))  # , a)]))
+im = im.resize((512, 512))
+im.save(args.out)

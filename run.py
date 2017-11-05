2dd53f561b84a215016070ed5c363ef1267deec2
ErikBoesen <me@erikboesen.com>
2017-11-05 16:48:27 -0500

    Support varied degrees of compression

--- run.py
+++ run.py
@@ -5,16 +5,22 @@ import argparse
 parser = argparse.ArgumentParser(description='Generate pixel portraits from an image.')
 parser.add_argument('file', type=str, help='Input file.')
 parser.add_argument('-out', type=str, default='', help='Output file.')
+parser.add_argument('-compression', type=int, default=20, help='Intensity of color compression. 10-50 recommended.')
 args = parser.parse_args()
+
+COMPRESSION = args.compression
 
 im = Image.open(args.file)
 im = im.resize((64, 64))
 
-_, g, _ = np.array(im).T
-g = map(round, g)
-g *= 64
+r, g, b = np.array(im).T
+r //= COMPRESSION
+r *= COMPRESSION
+g //= COMPRESSION
+g *= COMPRESSION
+b //= COMPRESSION
+b *= COMPRESSION
 
-
-im = Image.fromarray(np.dstack((g.T, g.T, g.T)))  # , a)]))
+im = Image.fromarray(np.dstack((r.T, g.T, b.T)))
 im = im.resize((512, 512))
 im.save(args.out)
